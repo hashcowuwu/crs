0d41d6c40fbf61d1ec1d5e923089f96e9b8a18b2
ZeroKaze <zerokaze404@gmail.com>
2025-05-17 15:43:45 +0800

    fix

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ import { createServer } from "http";
 
 interface ServerToClientEvents {
   noArg: () => void;
+  message: (message: string) => void;
+
   basicEmit: (a: number, b: string, c: Buffer) => void;
   withAck: (d: string, callback: (e: number) => void) => void;
 }
 
-interface ClientToServerEvents {
-  hello: () => void;
-}
+
 
 interface InterServerEvents {
   ping: () => void;
@@ -20,10 +20,35 @@ interface SocketData {
   name: string;
   age: number;
 }
-
+const httpServer = createServer();
 const io = new Server<
   ClientToServerEvents,
   ServerToClientEvents,
   InterServerEvents,
   SocketData
->();
+>(httpServer, {
+    cors: {
+        origin: ["http://localhost:3000"],
+    },
+});
+
+interface ClientToServerEvents {
+        message: (message: string) => void;
+}
+
+
+io.on("connection", (socket) => {
+  console.log(socket.id + "connected!");
+  socket.on("message", (message) => {
+    //监听message
+    console.log(`${socket.id}:${message}`);
+    socket.broadcast.emit(
+      "message",
+      JSON.stringify({
+        user: socket.id,
+        message: message,
+      }),
+    );
+  });
+});
+io.listen(4000);
