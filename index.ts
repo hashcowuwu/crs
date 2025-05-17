2d71f10c8dc2d8f24b8242da667558bf41908d02
ZeroKaze <zerokaze404@gmail.com>
2025-05-17 15:33:58 +0800

    fix

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
+
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>();
